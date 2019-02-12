5e9d546f99848959fa598428a5ddc3ed3e8aa3ec
Thomas Chappel <thomas.chappel4@gmail.com>
2019-02-12 11:33:28 +0100

    initial setup with create react app, typescript and Bulma

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import React, {Component} from 'react'
+import './App.css'
+import 'bulma/css/bulma.css'
+
+class App extends Component {
+    render() {
+        return (
+            <div className="App">
+                <a className="button">Button</a>
+            </div>
+        );
+    }
+}
+
+export default App;
