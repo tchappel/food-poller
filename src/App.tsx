66def1b404989b8c995d1c387ad7dbd296c39767
Thomas Chappel <thomas.chappel4@gmail.com>
2019-02-17 11:28:43 +0100

    remove App.css fix path to appContext

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,20 @@
-import React, {Component} from 'react'
-import './App.css'
-import 'bulma/css/bulma.css'
+import React, { Component } from "react";
+import "bulma/css/bulma.css";
 import SnackCardsDisplayer from "./components/SnackCardsDisplayer";
 import Header from "./components/Header";
-import AppContextProvider from "./Context";
+import AppContextProvider from "./appContext";
 
 class App extends Component {
-
-    render() {
-        return (
-            <div className="App">
-                <AppContextProvider>
-                    <Header />
-                    <SnackCardsDisplayer />
-                </AppContextProvider>
-            </div>
-        );
-    }
+  render() {
+    return (
+      <div className="App">
+        <AppContextProvider>
+          <Header />
+          <SnackCardsDisplayer />
+        </AppContextProvider>
+      </div>
+    );
+  }
 }
 
 export default App;
